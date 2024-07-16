1b2cec0122d325a80e669ffc77a3a7b54c6b3d44
Rishabh Kumar <rkv225@gmail.com>
2024-07-16 18:43:04 +0530

    promise methods

--- 15_promises/15_promises.md
+++ 15_promises/15_promises.md
@@ -1,13 +1,13 @@
 # Promises
 
 ## Definition
-
-Promise is a proxy for a value not necessarily known when the promise is created. 
+Promise is a proxy for a value not necessarily known when the promise is created.
 
 A Promise is in one of these states:
 1. pending: initial state or executing state
-2. fulfilled: means operation was successfull
-3. rejected: means that the operation failed
+2. settled: it means that the promise has finished execution. This leads to one of the following state:
+    - fulfilled/success/resolved: means operation was successfull
+    - rejected/failure: means that the operation failed
 
 Promises also solve the problem of callback hell by promise chaining.
 
@@ -22,3 +22,39 @@ let promise = new Promise((resolve, reject) => {
     }
 });
 ```
+
+## Promise methods
+Takes iterables as an input. Noramlly an array of promises is passed as parameter.
+
+### Promise.all
+This makes parallel API calls and wait for all of them to finish.
+- If all of them finish successfully then we get an array of results on index based on promise respectively.
+- If any of the promise fails then the output will be thrown as error and other promises are not awaited(although they are not cancelled).
+
+```js
+//output format
+['res1', 'res2', 'res3']
+```
+
+### Promise.allSettle
+This makes parallel API calls and waits for all the promises to settle. Irrespective of promise state, we get an array containing result or error on respective index.
+
+```js
+// output format
+[
+    {
+        status: 'fulfilled', 
+        value: ''
+    },
+    {
+        status: 'rejected',
+        reason: ''
+    }
+]
+```
+
+### Promise.race
+It return result from the first promise that gets settled. If first settled promise gives out result then that promise's result is returned else if it fails then that promise's error if returned.
+
+### Promise.any
+It return result result of first successfull promise. In case all of the promise fails then the result will be an `aggregate error` i.e. an array of all the errors.
--- 15_promises/index.js
+++ 15_promises/index.js
@@ -59,3 +59,129 @@ createOrder(cart).then(function(orderId) {
 }).catch((err) => {
     console.log(err.message);
 });
+
+// Demonstrating the use of promise methods
+
+function p1s() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("P1 success");
+        }, 3000);
+    });
+}
+
+function p1f() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject("P1 fail");
+        }, 3000);
+    });
+}
+
+function p2s() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("P2 success");
+        }, 1000);
+    });
+}
+
+function p2f() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject("P2 fail");
+        }, 1000);
+    });
+}
+
+function p3s() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("P3 success");
+        }, 2000);
+    });
+}
+
+function p3f() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject("P3 fail");
+        }, 2000);
+    });
+}
+
+Promise.all([p1s(), p2s(), p3s()]).then(res => {
+    console.log('Output from Promise.all([p1s(), p2s(), p3s()]):', res);
+});
+// output: ['P1 success', 'P2 success', 'P3 success']
+
+Promise.all([p1s(), p2f(), p3s()]).then(res => {
+    console.log('Output from Promise.all([p1s(), p2f(), p3s()]):', res);
+}).catch(err => {
+    console.error('Error from Promise.all([p1s(), p2f(), p3s()]):', err);
+});
+// output: P2 fail
+
+Promise.allSettled([p1s(), p2f(), p3f()]).then(res => {
+    console.log('Output from Promise.allSettled([p1s(), p2f(), p3f()]):', res);
+});
+/*
+output:
+[
+    {
+        "status": "fulfilled",
+        "value": "P1 success"
+    },
+    {
+        "status": "rejected",
+        "reason": "P2 fail"
+    },
+    {
+        "status": "rejected",
+        "reason": "P3 fail"
+    }
+]
+*/
+
+Promise.race([p1f(), p2f(), p3f()]).then(res => {
+    console.log('Output from Promise.race([p1f(), p2f(), p3f()]):', res);
+}).catch(err => {
+    console.error('Error from Promise.race([p1f(), p2f(), p3f()]):', err);
+});
+// output: P2 fail
+
+Promise.race([p1f(), p2s(), p3s()]).then(res => {
+    console.log('Output from Promise.race([p1f(), p2s(), p3s()]):', res);
+}).catch(err => {
+    console.error('Error from Promise.race([p1f(), p2s(), p3s()]):', err);
+});
+// output: P2 success
+
+Promise.any([p1f(), p2f(), p3s()]).then(res => {
+    console.log('Output from Promise.any([p1f(), p2f(), p3s()]):', res);
+}).catch(err => {
+    console.error('Error from Promise.any([p1f(), p2f(), p3s()]):', err);
+});
+// output: P3 success
+
+Promise.any([p1f(), p2f(), p3f()]).then(res => {
+    console.log('Output from Promise.any([p1f(), p2f(), p3f()]):', res);
+}).catch(err => {
+    console.error('Error from Promise.any([p1f(), p2f(), p3f()]):', err);
+    console.log('Errors from Promise.any([p1f(), p2f(), p3f()]):', err.errors);
+});
+/*
+output: AggregateError: All promises were rejected
+[
+    "P1 fail",
+    "P2 fail",
+    "P3 fail"
+]
+*/
+
+Promise.any([p1f(), p2s(), p3s()]).then(res => {
+    console.log('Output from Promise.any([p1f(), p2s(), p3s()]):', res);
+}).catch(err => {
+    console.error('Error from Promise.any([p1f(), p2s(), p3s()]):', err);
+});
+// output: P2 success
